69e9717910dcd3b11f9578bbbc0764c4bb71c0c9
moosecoop <cooper@makhijani.us>
2017-04-25 18:46:35 -0700

    merged /docs into master

---
+++ commands/4chan.js
@@ -0,0 +1,30 @@
+const Discord = require('discord.js');
+exports.run = (client, msg) => {
+    var msgEmbed = new Discord.RichEmbed()
+        .setTitle('4chan')
+        .setThumbnail('http://orig04.deviantart.net/74de/f/2012/155/d/1/4chan_logo_hq_by_michaudotcom-d529rdh.png')
+        .addField('4chan', 'is the place where the spiciest memes are')
+        .setURL('https://4chan.org')
+        .setColor("#039112");
+
+    msg.channel.sendEmbed(
+        msgEmbed, ''
+    )
+};
+
+exports.conf = {
+    enabled: true,
+    runIn: ["text"],
+    aliases: ["chan"],
+    permLevel: 0,
+    botPerms: [],
+    requiredFuncs: [],
+};
+
+exports.help = {
+    name: "4chan",
+    description: "link to 4chan",
+    usage: "",
+    usageDelim: "",
+    extendedHelp: "",
+};
---
+++ commands/mem.js
@@ -0,0 +1,37 @@
+const Discord = require('discord.js');
+exports.run = (client, msg) => {
+    var memes = [
+        "0.jpg",
+        "1.jpg",
+        "2.jpg",
+        "3.jpg",
+        "4.jpg",
+        "5.jpg",
+        "6.jpg",
+        "7.jpg",
+        "8.jpg",
+        "9.png",
+        "10.jpg",
+        "11.png"
+    ]
+
+    var mem = memes[Math.floor(Math.random() * memes.length) + 1];
+    msg.channel.sendFile(`././mems/${mem}`);
+};
+
+exports.conf = {
+    enabled: true,
+    runIn: ["text"],
+    aliases: ["memes", "memz", "mems"],
+    permLevel: 0,
+    botPerms: [],
+    requiredFuncs: [],
+};
+
+exports.help = {
+    name: "mem",
+    description: "sends a random mem",
+    usage: "",
+    usageDelim: "",
+    extendedHelp: "",
+};
