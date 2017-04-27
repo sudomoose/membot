bcf9c6cc71f002f53763596e108603052c148043
moosecoop <cooper@makhijani.us>
2017-04-26 18:05:10 -0700

    guildCreate now sends join text message instead of embed

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -1,17 +1,7 @@
-const Discord = require('discord.js');
 exports.run = (client, guild) => {
-
-  var messageEmbed = new Discord.RichEmbed()
-        .setTitle('membot')
-        .setImage('https://lh3.googleusercontent.com/j5fD3m5qXRNtGYDuajhEtS_etFvU8FE5PogmqTY2hrshDG0_urf_UBeVAyJljoCxdf4=w300')
-        .setAuthor('membot')
-        .addField('Creator', 'Moosecoop#3054')
-        .setURL('https://moosecoop.github.io/membot/')
-        .setFooter('© MooseHub 2017 - membot by Moosecoop#3054')
-        .description('membot is a Discord bot for anyone who wants more memes in their life (or Discord server).')
-        .setColor("#039112");
-
-    guild.defaultChannel.sendEmbed(
-        messageEmbed, '.'
-    );
+    var channel = guild.defaultChannel;
+    var name = guild.name;
+    if(guild.availble) {
+        channel.sendMessage(`whats poppin skrubs of ${name}... try \`mem help\` for commands!`);
+    }
 };
